21492601e7ec8ff4f9b251b403313db536e39f75
Dereck Luiz dos Santos <107744801+Dereckye@users.noreply.github.com>
2023-05-18 18:54:28 -0300

    Update script.js

--- Projeto OcarinaOfTime/script.js
+++ Projeto OcarinaOfTime/script.js
@@ -11,8 +11,6 @@ document.querySelector('.composer button').addEventListener('click', () => {
     }
 
 })
-    
-
 function playSound(sound){
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
@@ -28,10 +26,11 @@ function playSound(sound){
         }, 300);
     }
 }
+
+//
+
 function playComposition(songArray){
     let wait = 0;
-
-
     for(let songItem of songArray){
         setTimeout(()=>{
             playSound(`key${songItem}`);
@@ -39,3 +38,26 @@ function playComposition(songArray){
         wait += 250;
     }
 }
+
+const sequence = ['s', 'a', 'e','s', 'a', 'e'];
+
+const audio1 = new Audio('sounds/Song_Correct.wav');
+const audio2 = new Audio('sounds/Eponas Song.mp3');
+
+let pressedKeys = [];
+
+document.addEventListener('keydown', event => {
+
+  pressedKeys.push(event.key);
+
+  if (pressedKeys.join('') === sequence.join('')) {
+ 
+    audio1.play();
+   
+    setTimeout(() => {
+      audio2.play();
+
+      pressedKeys = [];
+    }, 1000);
+  }
+});
